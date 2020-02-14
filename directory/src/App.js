37a6dafa4215f3b95d8da8f0fa49d0fae77f6ca2
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-02-14 12:16:56 -0500

    Added react-router-dom dependency. Developed filters component for homepage

--- directory/src/App.js
+++ directory/src/App.js
@@ -1,17 +1,32 @@
 import React from "react";
 import "./App.scss";
+import { NavLink } from "react-router-dom";
 
-import Directory from "./components/Directory/Directory"
+import Directory from "./components/Directory/Directory";
 
 function App() {
   return (
     <div className="app">
+
       <header>
         <div className="navigation-menu">
           <p>Logo</p>
         </div>
       </header>
-      <Directory />
+
+      <div className="home-content">
+        <h2>Directory</h2>
+        <div className="filters">
+          <NavLink to={"/"}>
+            All
+          </NavLink>
+          <NavLink to={"/merchants"}>
+            Merchants
+          </NavLink>
+        </div>
+        <Directory />
+      </div>
+      
     </div>
   );
 }
