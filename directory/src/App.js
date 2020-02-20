5fc4282d972a08a4d3c626e79bc40ecc143170a9
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-02-20 10:11:19 -0500

    Filters moved under subtitle

--- directory/src/App.js
+++ directory/src/App.js
@@ -7,6 +7,7 @@ import btcPayLogo from "./images/btcpay-directory-logo.svg"
 function App() {
   const green = "#51B13E";
   const black = "#262626";
+  const darkGreen = "#1E7A44";
   const pickedColor = green;
 
   return (
@@ -14,7 +15,80 @@ function App() {
       <header>
         <div className="navigation-menu">
           <img src={btcPayLogo} />
-          <div className="filters">
+          {/* <div className="filters">
+            <NavLink
+              className="filter"
+              exact
+              activeStyle={{ color: pickedColor }}
+              to={"/"}
+            >
+              ALL
+            </NavLink>
+            <NavLink
+              className="filter"
+              exact
+              activeStyle={{ color: pickedColor }}
+              to={"/apps"}
+            >
+              APPS
+            </NavLink>
+            <NavLink
+              className="filter"
+              exact
+              activeStyle={{ color: pickedColor }}
+              to={"/hosts"}
+            >
+              HOSTS
+            </NavLink>
+            <NavLink
+              className="filter"
+              exact
+              activeStyle={{ color: pickedColor }}
+              to={"/merchants"}
+            >
+              MERCHANTS
+            </NavLink>
+            <NavLink
+              className="filter"
+              exact
+              activeStyle={{ color: pickedColor }}
+              to={"/non-profit"}
+            >
+              NON&#8209;PROFITS
+            </NavLink>
+          </div> */}
+        </div>
+        {/* <svg
+          className="navigation-gradient"
+          enable-background="new 0 0 100 7"
+          height="7"
+          viewBox="0 0 100 7"
+          width="100"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <linearGradient
+            id="a"
+            gradientUnits="userSpaceOnUse"
+            x1="50.1831"
+            x2="49.8171"
+            y1="8.7402"
+            y2="-1.7397"
+          >
+            <stop offset="0" stop-color="#0c0068" stop-opacity="0"></stop>
+            <stop stop-color="#0c0068" stop-opacity="0" offset=".5"></stop>
+            <stop offset="1" stop-color="#0c0068" stop-opacity=".2"></stop>
+          </linearGradient>
+          <path d="m0 0h100v7h-100z" fill="url(#a)" />
+          <path
+            d="m100 .916v-.916h-100v4c11.385-2 80.846-5.162 100-3.084z"
+            fill="#fff"
+          />
+        </svg> */}
+      </header>
+
+      <div className="home-content">
+        <h2>Projects and organizations using BTCPay Server</h2>
+        <div className="filters">
             <NavLink
               className="filter"
               exact
@@ -56,37 +130,6 @@ function App() {
               NON&#8209;PROFITS
             </NavLink>
           </div>
-        </div>
-        {/* <svg
-          className="navigation-gradient"
-          enable-background="new 0 0 100 7"
-          height="7"
-          viewBox="0 0 100 7"
-          width="100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <linearGradient
-            id="a"
-            gradientUnits="userSpaceOnUse"
-            x1="50.1831"
-            x2="49.8171"
-            y1="8.7402"
-            y2="-1.7397"
-          >
-            <stop offset="0" stop-color="#0c0068" stop-opacity="0"></stop>
-            <stop stop-color="#0c0068" stop-opacity="0" offset=".5"></stop>
-            <stop offset="1" stop-color="#0c0068" stop-opacity=".2"></stop>
-          </linearGradient>
-          <path d="m0 0h100v7h-100z" fill="url(#a)" />
-          <path
-            d="m100 .916v-.916h-100v4c11.385-2 80.846-5.162 100-3.084z"
-            fill="#fff"
-          />
-        </svg> */}
-      </header>
-
-      <div className="home-content">
-        <h2>Directory</h2>
         <Route exact path={"/"}>
           <Directory />
         </Route>
