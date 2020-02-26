399e41e8c1aa002bfd3342444d71c9f9282ec3e4
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-02-26 15:16:35 -0500

    Initial form structure developed

--- directory/src/components/NewEntry/NewEntry.js
+++ directory/src/components/NewEntry/NewEntry.js
@@ -4,7 +4,29 @@ import "./NewEntry.scss";
 function NewEntry() {
 
   return (
-    <p></p>
+    <form>
+      <label for="name">
+        Name
+        <input id="name" type="text" />
+      </label>
+      <label for="url">
+        URL
+        <input id="url" type="url" />
+      </label>
+      <label for="type">
+        Type
+        <select id="type">
+          <option selected value="app">App</option>
+          <option value="host">Host</option>
+          <option value="merchant">Merchant</option>
+          <option value="non-profit">Non-ptofit</option>
+        </select>
+      </label>
+      <label for="description">
+        Description
+        <input id="description" type="text" />
+      </label>
+    </form>
   );
 }
 
