d87fd8b8095634e5d9b46b1fa83499441eab25b8
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-02-26 17:14:02 -0500

    Form styling done

--- directory/src/components/NewEntry/NewEntry.js
+++ directory/src/components/NewEntry/NewEntry.js
@@ -1,32 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./NewEntry.scss";
 
 function NewEntry() {
-
   return (
-    <form>
-      <label for="name">
-        Name
-        <input id="name" type="text" />
-      </label>
-      <label for="url">
-        URL
-        <input id="url" type="url" />
-      </label>
-      <label for="type">
-        Type
-        <select id="type">
-          <option selected value="app">App</option>
-          <option value="host">Host</option>
-          <option value="merchant">Merchant</option>
-          <option value="non-profit">Non-ptofit</option>
-        </select>
-      </label>
-      <label for="description">
-        Description
-        <input id="description" type="text" />
-      </label>
-    </form>
+    <div className="newentry-container">
+      <h2>Organization Details</h2>
+      <div className="form-container">
+        <form>
+          <label for="name">
+            Name
+            <input id="name" type="text" placeholder="Your organization's name" />
+          </label>
+          <label for="url">
+            URL
+            <input id="url" type="url" placeholder="URL of your organization" />
+          </label>
+          <label for="type">
+            Type
+            <select id="type">
+              <option selected value="app">
+                App
+              </option>
+              <option value="host">Host</option>
+              <option value="merchant">Merchant</option>
+              <option value="non-profit">Non-ptofit</option>
+            </select>
+          </label>
+          <label for="description">
+            Description
+            <textarea className="description" id="description" />
+          </label>
+          <button>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
