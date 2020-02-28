57ea4ac502feba679cebf51460f0893bb5f78c17
Jonathan Erlich <jonathanerlich@Jonathans-MacBook-Pro.local>
2020-02-28 15:30:09 -0500

    Code cleanup

--- directory/src/App.js
+++ directory/src/App.js
@@ -25,75 +25,7 @@ function App() {
             </NavLink>
             {/* <img src={darkModeImg} /> */}
           </div>
-          {/* <div className="filters">
-            <NavLink
-              className="filter"
-              exact
-              activeStyle={{ color: pickedColor }}
-              to={"/"}
-            >
-              ALL
-            </NavLink>
-            <NavLink
-              className="filter"
-              exact
-              activeStyle={{ color: pickedColor }}
-              to={"/apps"}
-            >
-              APPS
-            </NavLink>
-            <NavLink
-              className="filter"
-              exact
-              activeStyle={{ color: pickedColor }}
-              to={"/hosts"}
-            >
-              HOSTS
-            </NavLink>
-            <NavLink
-              className="filter"
-              exact
-              activeStyle={{ color: pickedColor }}
-              to={"/merchants"}
-            >
-              MERCHANTS
-            </NavLink>
-            <NavLink
-              className="filter"
-              exact
-              activeStyle={{ color: pickedColor }}
-              to={"/non-profit"}
-            >
-              NON&#8209;PROFITS
-            </NavLink>
-          </div> */}
         </div>
-        {/* <svg
-          className="navigation-gradient"
-          enable-background="new 0 0 100 7"
-          height="7"
-          viewBox="0 0 100 7"
-          width="100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <linearGradient
-            id="a"
-            gradientUnits="userSpaceOnUse"
-            x1="50.1831"
-            x2="49.8171"
-            y1="8.7402"
-            y2="-1.7397"
-          >
-            <stop offset="0" stop-color="#0c0068" stop-opacity="0"></stop>
-            <stop stop-color="#0c0068" stop-opacity="0" offset=".5"></stop>
-            <stop offset="1" stop-color="#0c0068" stop-opacity=".2"></stop>
-          </linearGradient>
-          <path d="m0 0h100v7h-100z" fill="url(#a)" />
-          <path
-            d="m100 .916v-.916h-100v4c11.385-2 80.846-5.162 100-3.084z"
-            fill="#fff"
-          />
-        </svg> */}
       </header>
 
       <div className="main-content">
